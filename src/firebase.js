c07f22cb680cb732731a87516bb5260d2f5da690
Sai-Pranav-Tumu <saipranav.tumu@gmail.com>
2023-11-07 23:06:05 +0530

    'commit1'

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
+//   authDomain: "chat-ab746.firebaseapp.com",
+//   projectId: "chat-ab746",
+//   storageBucket: "chat-ab746.appspot.com",
+//   messagingSenderId: "901216368405",
+//   appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+// };
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuqkrwXPXKACyGxPmW7fOMkegxS9S3wX0",
+  authDomain: "chat-4f245.firebaseapp.com",
+  projectId: "chat-4f245",
+  storageBucket: "chat-4f245.appspot.com",
+  messagingSenderId: "1055497460136",
+  appId: "1:1055497460136:web:60ea7e242d9c9e0721a59d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore()
