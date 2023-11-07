025a45cab616e49aa5ee1039c48172b69275adbd
Sai-Pranav-Tumu <95917798+Sai-Pranav-Tumu@users.noreply.github.com>
2023-11-07 23:29:05 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const {currentUser} = useContext(AuthContext)
   return (
     <div className='navbar'>
-      <span className="logo">Lama Chat</span>
+      <span className="logo">TSPchat</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
